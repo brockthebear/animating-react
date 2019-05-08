7838312e924ad91c7e01dfa94dbc7634c88b8d8d
Brock Boren <brock.boren@gmail.com>
2019-05-08 13:16:07 -0500

    Transition with mounting and unmounting components.

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,28 +1,26 @@
 import React, { useState } from 'react'
-import { useSpring, animated } from 'react-spring'
+import { useTransition, animated } from 'react-spring'
 
 const Toggle = () => {
 	const [isToggled, setToggle] = useState(false)
-	const { color, y } = useSpring({
-		color: isToggled ? 'tomato' : 'green',
-		y: isToggled ? 0 : 1,
+	// useTransition is useful for things like a transition or anything that wants
+	// to mount after the page has been rendered.
+	const transition = useTransition(isToggled, null, {
+		from: { opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
 	})
 
 	return (
 		<div>
-			<animated.h1
-				style={{
-					color,
-					transform: y
-						.interpolate({
-							range: [0, 0.25, 0.5, 0.75, 1],
-							output: [0, -25, -50, -100, -50],
-						})
-						.interpolate(y => `translate3d(0, ${y}px, 0)`),
-				}}
-			>
-				Hello
-			</animated.h1>
+			{transition.map(
+				({ item, key, props }) =>
+					item && (
+						<animated.h1 key={key} style={props}>
+							Hello
+						</animated.h1>
+					)
+			)}
 			<button onClick={() => setToggle(!isToggled)}>Toggle</button>
 		</div>
 	)
