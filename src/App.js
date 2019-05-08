8419e43081d23367fa5b42d747ea5d32c2ad8708
Brock Boren <brock.boren@gmail.com>
2019-05-08 12:44:49 -0500

    Use interpolation to animate Nav and Checkout page.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSpring, animated } from 'react-spring'
+import Nav from './Nav'
 import Toggle from './Toggle'
+import Checkout from './Checkout'
 import logo from './logo.svg'
 import './App.css'
 
 const App = () => {
+	const [isNavOpen, setNavOpen] = useState(false)
+	const navAnimation = useSpring({
+		transform: isNavOpen ? `translate3d(0, 0, 0) scale(1)` : `translate3d(100%, 0, 0) scale(0.6)`,
+	})
 	const fade = useSpring({ from: { opacity: 0 }, opacity: 1 })
 
 	return (
 		<animated.div className="App" style={fade}>
 			<header className="App-header">
 				<img src={logo} className="logo" alt="Logo" />
-				<button className="menu-button">Menu</button>
+				<button onClick={() => setNavOpen(!isNavOpen)} className="menu-button">
+					Menu
+				</button>
+				{/* <Nav style={navAnimation} /> */}
 			</header>
 			<main>
 				<Toggle />
+				<Checkout isOpen={isNavOpen} />
 			</main>
 		</animated.div>
 	)
