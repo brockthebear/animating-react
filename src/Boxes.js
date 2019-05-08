39e6414b776af2464e6398d537052fcf70857e5d
Brock Boren <brock.boren@gmail.com>
2019-05-08 17:02:06 -0500

    Chain together different springs with useChain().

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,24 +1,67 @@
-import React, { useState } from 'react'
-import { animated, useTrail } from 'react-spring'
+import React, { useState, useRef } from 'react'
+import { animated, useSpring, useTrail, useTransition, useChain } from 'react-spring'
 
-const items = [1, 2, 3, 4, 5]
+const items = [1, 2, 3, 4, 5, 6]
 
 const Boxes = () => {
 	const [on, toggle] = useState(false)
-	const trail = useTrail(items.length, {
-		config: {
-			delay: 1000,
-		},
-		opacity: on ? 0 : 1,
-		transform: on ? 'scale(0.3)' : 'scale(1)',
+
+	const springRef = useRef()
+	const { size } = useSpring({
+		ref: springRef,
+		from: { size: '20%' },
+		to: { size: on ? '100%' : '20%' },
 	})
 
+	const transitionRef = useRef()
+	// const trail = useTrail(items.length, {
+	// 	ref: transitionRef,
+	// 	config: {
+	// 		delay: 1000,
+	// 	},
+	// 	to: {
+	// 		opacity: on ? 1 : 0,
+	// 		transform: on ? 'scale(1)' : 'scale(0)',
+	// 	},
+	// 	from: {
+	// 		opacity: 0,
+	// 		transform: 'scale(0)',
+	// 	},
+	// })
+
+	const transition = useTransition(on ? items : [], item => item, {
+		ref: transitionRef,
+		trail: 400 / items.length,
+		from: {
+			opacity: 0,
+			transform: 'scale(0)',
+		},
+		enter: {
+			opacity: 1,
+			transform: 'scale(1)',
+		},
+		leave: {
+			opacity: 0,
+			transform: 'scale(0)',
+		},
+	})
+
+	useChain(on ? [springRef, transitionRef] : [transitionRef, springRef])
+
 	return (
-		<div className="boxes-grid">
-			<button onClick={() => toggle(!on)}>Toggle Boxes</button>
-			{trail.map(animation => (
-				<animated.div className="box" style={animation} />
-			))}
+		<div className="full-height">
+			<animated.div
+				className="boxes-grid-two"
+				style={{ width: size, height: size }}
+				onClick={() => toggle(!on)}
+			>
+				{/* {trail.map(animation => (
+					<animated.div className="box-two" style={animation} />
+				))} */}
+				{transition.map(({ item, key, props: animationProps }) => (
+					<animated.div className="box-two" key={key} style={animationProps} />
+				))}
+			</animated.div>
 		</div>
 	)
 }
