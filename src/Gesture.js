4a73896a89ec812e37533d3a472c47b23ab1174d
Brock Boren <brock.boren@gmail.com>
2019-05-08 16:13:30 -0500

    Restrict drag to horizontal.

--- src/Gesture.js
+++ src/Gesture.js
@@ -3,15 +3,19 @@ import { useGesture } from 'react-with-gesture'
 import { animated, useSpring, config } from 'react-spring'
 
 const Gesture = () => {
-	const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }))
+	const [{ x }, set] = useSpring(() => ({ x: 0 }))
 
-	const bind = useGesture(({ down, delta }) => {
+	const bind = useGesture(values => {
 		// set() can be used to set values (like setState) outside of the spring.
 		// the spring will update with the new values in an animated way.
-		set({
-			xy: down ? delta : [0, 0],
-			config: config.wobbly,
-		})
+		let { down, delta } = values
+		console.log(values)
+		if (down) {
+			set({
+				x: down ? delta[0] : 0,
+				config: config.wobbly,
+			})
+		}
 	})
 
 	return (
@@ -19,7 +23,8 @@ const Gesture = () => {
 			{...bind()}
 			className="box"
 			style={{
-				transform: xy.interpolate((x, y) => `translate3d(${x}px, ${y}px, 0)`),
+				opacity: x.interpolate({ map: Math.abs, range: [0, 400], output: [1, 0] }),
+				transform: x.interpolate((x, y) => `translate3d(${x}px, 0, 0)`),
 			}}
 		/>
 	)
