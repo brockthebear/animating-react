8c95ccb86c5004534bef020a566b7eca284be63c
Brock Boren <brock.boren@gmail.com>
2019-05-08 14:26:29 -0500

    Toggle styling for pointer events depending on Modal visibility.

--- src/Modal.js
+++ src/Modal.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { animated, useTransition } from 'react-spring'
 
-const Modal = ({ animation, closeModal }) => {
+const Modal = ({ animation, pointerEvents, closeModal }) => {
 	return (
-		<div className="modal">
+		<div className="modal" style={{ pointerEvents }}>
 			<animated.div className="modal-card" style={animation}>
 				<button onClick={closeModal}>Close</button>
 				<h1>Modal</h1>
@@ -15,16 +15,23 @@ const Modal = ({ animation, closeModal }) => {
 const ModalWrapper = () => {
 	const [on, toggle] = useState(false)
 	const transition = useTransition(on, null, {
-		from: { opacity: 0, transform: `translate3d(0, -40px, 0)` },
-		enter: { opacity: 1, transform: `translate3d(0, 0, 0)` },
-		leave: { opacity: 0, transform: `translate3d(0, -40px, 0)` },
+		from: { opacity: 0, transform: 'translate3d(0, -40px, 0)' },
+		enter: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
+		leave: { opacity: 0, transform: 'translate3d(0, -40px, 0)' },
 	})
+	const pointerEvents = on ? 'all' : 'none'
 
 	return (
 		<div>
 			{transition.map(
 				({ item, key, props: transitionProps }) =>
-					item && <Modal animation={transitionProps} closeModal={() => toggle(false)} />
+					item && (
+						<Modal
+							animation={transitionProps}
+							pointerEvents={pointerEvents}
+							closeModal={() => toggle(false)}
+						/>
+					)
 			)}
 			<button onClick={() => toggle(!on)}>Open</button>
 		</div>
