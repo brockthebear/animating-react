607b4039e95b2fb69e5fe6e3e03777910642c037
Brock Boren <brock.boren@gmail.com>
2019-05-08 12:14:00 -0500

    Animate Toggle properties.

--- src/Toggle.js
+++ src/Toggle.js
@@ -4,7 +4,10 @@ import { useSpring, animated } from 'react-spring'
 const Toggle = () => {
 	const [isToggled, setToggle] = useState(false)
 	const fade = useSpring({
-		opacity: isToggled ? 1 : 0,
+		// opacity: isToggled ? 1 : 0,
+		color: isToggled ? 'tomato' : 'green',
+		// fontSize: isToggled ? '2rem' : '5em',
+		transform: isToggled ? 'translate3d(0,0,0)' : 'translate3d(0,-50px,0)',
 	})
 
 	return (
